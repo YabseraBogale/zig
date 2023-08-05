3a07f184668741a8ecfe2064f096c402da8d00a6
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-08-05 20:19:55 +0300

    Made public function

--- addition/src/main.zig
+++ addition/src/main.zig
@@ -1,6 +1,9 @@
 const std = @import("std");
 
 pub fn main() !void {
-    var num: i32 = 40;
-    std.debug.print("number is .{}\n", .{num});
+    std.debug.print("the sum of two number is {}\n", .{sum(1, 2)});
+}
+
+pub fn sum(num1: i32, num2: i32) i32 {
+    return num1 + num2;
 }
