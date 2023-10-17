98908398fe90118754b4477a3b767343db443ecb
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-17 18:20:51 +0300

    i am back

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Starting ZIG 
+
+done with Rust and i sucks ass when it come to the build size.
--- hello.zig
+++ hello.zig
@@ -2,4 +2,5 @@ const std=@import("std").debug.print;
 
 pub fn main()!void{
     std("hello world\n",.{});
+    std("I am back and the tools have gone better than excpted\n",.{});
 }
