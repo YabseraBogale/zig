03ee76a1b99a859d5e6c2d2ee3beeee17788f1a5
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-19 16:51:34 +0300

    standard input not now

--- array.zig
+++ array.zig
@@ -2,5 +2,8 @@ const print = @import("std").debug.print;
 
 pub fn main() void {
     var x = [_]i32{ 1, 2, 3 };
-    print("The array in x is {x}", .{x});
+    print("The array in x is {x} \n", .{x});
+    for (x) |value| {
+        print("Array Value {}\n", .{value});
+    }
 }
---
+++ take_data.zig
@@ -0,0 +1,8 @@
+const std = @import("std");
+
+pub fn main() void {
+    //std.debug.print("Enter number: ", .{});
+    //var x = std.io.getStdIn().reader();
+    // _ = x;
+    // not now
+}
