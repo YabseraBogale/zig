9dce1edf2b6180e031208af215c54184b98c6315
YabseraBogale <yabserabogalercd30932013@gmail.com>
2023-10-20 19:17:05 +0300

    ok for numbers and utf characters

--- func.zig
+++ func.zig
@@ -1,6 +1,16 @@
+// the languge needs more work
+
 const std = @import("std").debug.print;
 pub fn main() void {}
 
-pub fn hello() s {
-    return "hello from function";
+pub fn hello() i32 {
+    return 54;
+}
+
+pub fn character() u8 {
+    return 'h';
+}
+
+pub fn world() f32 {
+    return 3.2;
 }
